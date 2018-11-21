561c84a9445c2182d22e3f233efb3cd154e3df8b
anayeremeiko <nastyaeremeyko@gmail.com>
2018-11-22 00:02:44 +0300

    tests

---
+++ No8.Solution.Tests/LoggerTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace No8.Solution.Tests
+{
+    [TestFixture]
+    public class LoggerTests
+    {
+        [TestCase("logging.txt")]
+        public void Logger_Log(string name)
+        {
+            Logger log = new Logger(name);
+            PrintersRepository repo = new PrintersRepository();
+            PrinterManager manager = new PrinterManager(repo);
+            var printer = new CanonPrinter("1G");
+            log.Register(manager);
+            manager.Add(printer);
+            using (File.CreateText("test1.txt"))
+            {
+            }
+            manager.Print(printer, "test1.txt");
+            string expected = "Print started\r\nPrint finished.\r\nPrinted on Canon\r\n";
+            string actual;
+
+            using (var logFile = File.OpenText(name))
+            {
+                actual = logFile.ReadToEnd();
+            }
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Logger_ThrowArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => new Logger(null));
+        }
+    }
+}
--- No8.Solution.Tests/PrinterManagerTests.cs
+++ No8.Solution.Tests/PrinterManagerTests.cs
@@ -1,10 +1,8 @@
-﻿using NUnit.Framework;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace No8.Solution.Tests
 {
@@ -27,7 +25,51 @@ namespace No8.Solution.Tests
             PrinterManager manager = new PrinterManager(repo);
             var printer = new CanonPrinter("123x");
             manager.Add(printer);
-            Assert.Throws<FileNotFoundException>(() => manager.Print(printer, "text.txt"));
+            Assert.Throws<FileNotFoundException>(() => manager.Print(printer, "error.txt"));
+        }
+
+        [Test]
+        public void Print_PrintFile()
+        {
+            PrintersRepository repo = new PrintersRepository();
+            PrinterManager manager = new PrinterManager(repo);
+            var printer = new CanonPrinter("123x");
+            manager.Add(printer);
+            string text = "Some text...";
+            using (var file = File.CreateText("text.txt"))
+            {
+                file.WriteLine(text);
+            }
+
+            var bytes = Encoding.UTF8.GetBytes(text + "\r\n");
+            var expected = new List<string>(bytes.Length);
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                if (i == 0)
+                {
+                    expected.Add(bytes[i].ToString());
+                    continue;
+                }
+                expected.Add("\n" + bytes[i]);
+            }
+
+            var actual = manager.Print(printer, "text.txt").Trim().Split('\r');
+
+            CollectionAssert.AreEqual(expected.ToArray(), actual);
+        }
+
+        [Test]
+        public void GetAllPrinters_ReturnAvailablePrinters()
+        {
+            PrintersRepository repo = new PrintersRepository();
+            PrinterManager manager = new PrinterManager(repo);
+            manager.Add(new CanonPrinter("1G"));
+            manager.Add(new EpsonPrinter("231"));
+            manager.Add(new CanonPrinter("5K"));
+            manager.Add(new EpsonPrinter("K505U"));
+
+            CollectionAssert.AreEqual(new Printer[] { new CanonPrinter("1G"), new EpsonPrinter("231"), new CanonPrinter("5K"), new EpsonPrinter("K505U") }, manager.GetAllPrinters(), Comparer<Printer>.Create((p1, p2) => string.Compare(p1.Name, p2.Name, StringComparison.Ordinal)));
+            CollectionAssert.AreEqual(new Printer[] { new CanonPrinter("1G"), new EpsonPrinter("231"), new CanonPrinter("5K"), new EpsonPrinter("K505U") }, manager.GetAllPrinters(), Comparer<Printer>.Create((p1, p2) => string.Compare(p1.Model, p2.Model, StringComparison.Ordinal)));
         }
     }
 }
